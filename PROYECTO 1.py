ea31c46fa4782916e8f66f2fd5ad7fd9616dfa32
AnderSeven <andersonmanoloalvaradoalvarez@gmail.com>
2025-08-15 23:30:00 -0600

    Se crea el menu en la rama principal

--- PROYECTO 1.py
+++ PROYECTO 1.py
@@ -1,1 +1,21 @@
-#a
+opciones = 0
+a = False
+while a == False:
+    print("-----Menu-----\n1. Registrar producto\n2. Listar productos\n3. Buscar productos\n4. Actualizar producto\n5. Eliminar producto\n6. Salir")
+    opciones = int(input("Elija una opcion: "))
+    match opciones:
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case 5:
+            pass
+        case 6:
+            print("Gracias por usar el sistema")
+            a = True
+        case _:
+            print("Opcion invalida")
