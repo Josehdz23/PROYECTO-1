e27a83f1473a8aa8fa8772ccb8a45b27e7b385f6
AnderSeven <andersonmanoloalvaradoalvarez@gmail.com>
2025-08-15 23:40:17 -0600

    se crea el submenu para listar los productos

--- PROYECTO 1.py
+++ PROYECTO 1.py
@@ -1,13 +1,28 @@
 opciones = 0
 a = False
 while a == False:
-    print("-----Menu-----\n1. Registrar producto\n2. Listar productos\n3. Buscar productos\n4. Actualizar producto\n5. Eliminar producto\n6. Salir")
+    print("\n-----Menu-----\n1. Registrar producto\n2. Listar productos\n3. Buscar productos\n4. Actualizar producto\n5. Eliminar producto\n6. Salir")
     opciones = int(input("Elija una opcion: "))
     match opciones:
         case 1:
             pass
         case 2:
-            pass
+            opciones1 = 0
+            b = False
+            while b == False:
+                print("\n----Listar Productos----\n1. Por nombre\n2. Por precio\n3. Por stock\n4. Regresar")
+                opciones1 = int(input("Elija una opcion: "))
+                match opciones1:
+                    case 1:
+                        pass
+                    case 2:
+                        pass
+                    case 3:
+                        pass
+                    case 4:
+                        b = True
+                    case _:
+                        print("Opcion invalida")
         case 3:
             pass
         case 4:
