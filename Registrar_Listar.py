f3f96ccc24f54994001e6ec34581602689cd3f3d
AnderSeven <andersonmanoloalvaradoalvarez@gmail.com>
2025-08-16 00:16:19 -0600

    era productos xd

--- Registrar_Listar.py
+++ Registrar_Listar.py
@@ -1,4 +1,4 @@
-class registro():
+class productos():
     def __init__(self, codigo, nombre, categoria, precio, stock):
         self.codigo = codigo
         self.nombre = nombre
@@ -8,3 +8,4 @@ class registro():
 
     def info(self):
         return f"Codigo: {self.codigo}, Nombre: {self.nombre}, Categoria: {self.categoria}, Precio: {self.precio}, Stock: {self.precio}"
+
