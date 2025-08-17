28722bf465dc9fb95d0d22a99eebbe9ceabc0370
AnderSeven <andersonmanoloalvaradoalvarez@gmail.com>
2025-08-16 21:59:00 -0600

    se crea la rama buscar actualizar, y se crea la clase buscar

---
+++ Buscar_Actualizar.py
@@ -0,0 +1,17 @@
+from Registrar_Listar import registro_productos
+
+class Buscar():
+    def __init__(self, registro):
+        self.registro = registro
+
+    def buscar_codigo(self, codigo):
+        if codigo in self.registro.diccionario_productos:
+            return self.registro.diccionario_productos[codigo].info_productos()
+        else:
+            return "No"
+
+    def buscar_nombre(self, nombre):
+        for producto in self.registro.diccionario_productos.values():
+            if producto.nombre.lower() == nombre.lower():
+                return producto.info_productos()
+        return "No se encontraron productos con ese nombre"
